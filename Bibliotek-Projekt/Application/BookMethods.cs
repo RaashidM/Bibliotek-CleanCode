8322b6f2d705ed01fc9a071d57eee51a3f84d22e
RaashidM <raashidm24@gmail.com>
2024-11-20 17:01:53 +0100

    Created BookMetods and added test for BookMethod.

---
+++ Bibliotek-Projekt/Application/BookMethods.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+
+namespace Application
+{
+    public class BookMethods
+    {
+        public static Book AddNewBook()
+        {
+            Book newBookToAdd = new Book(1, "Harry Potter", "Fantasy Book");
+            return newBookToAdd;
+        }
+    }
+}
